52c5d0f5037542be017eab984a79175f9c33416a
rcorenti <corentin.reyleroux@ynov.com>
2025-02-26 20:15:47 +0100

    update Garden.py with get_name_plants

--- Garden.py
+++ Garden.py
@@ -9,10 +9,14 @@ class Garden:
         for plant in self.plants:
             plant.add_day()
 
+    def get_name_plants(self):
+        result = ""
+        for plant in self.plants:
+            result += plant.get_name()
+        print(result)
 
 
     def get_report(self):
         report = ""
         for plant in self.plants: report += f'{plant}\n\n'
-
         return report
