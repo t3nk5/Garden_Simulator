68e105203eae36a457b048a3b5b3a265ec7e694a
rcorenti <corentin.reyleroux@ynov.com>
2025-02-26 20:22:55 +0100

    update main.py

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from Game.Game import Game
 NUMBER_OF_PLANTS = 2
 
 def main():
-    #test()
 
     player_name = input("Enter your name: ")
     game = Game(player_name)
 
     game.start(NUMBER_OF_PLANTS)
+    game.display_garden()
+
+    game.choose_action()
     game.display_garden()
 
 
@@ -23,3 +25,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
