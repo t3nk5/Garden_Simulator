17fb2f8105e475911352d0836cd2f6227cd5d8df
rcorenti <corentin.reyleroux@ynov.com>
2025-02-28 20:26:10 +0100

    add event.py

---
+++ Events/event.py
@@ -0,0 +1,34 @@
+import random
+from Events.enum_events import EventType
+from Events.intensity import Intensity
+
+
+
+
+class Event:
+    def __init__(self):
+        self.event_type  = self.random_event()
+        self.intensity = self.random_intensity()
+
+
+    @staticmethod
+    def get_random_event() -> EventType:
+        return random.choice(list(EventType))
+
+    def random_event(self):
+        event = self.get_random_event()
+        print(f"OHH noo there is a : {event.name}")
+        return event
+
+    @staticmethod
+    def random_intensity() -> Intensity:
+        return random.choice(list(Intensity))
+
+
+    def __str__(self) -> str:
+        return (
+            f"🌱 {self.event_type.name.capitalize()} \n"
+            f"☠️ {self.intensity.name.capitalize()} \n"
+        )
+
+
